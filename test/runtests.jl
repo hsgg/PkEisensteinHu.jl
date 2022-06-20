02735b1be7cc69648fdf7570112e6b98d5b8dcda
Henry Gebhardt <hsggebhardt@fastmail.com>
2022-06-20 10:47:49 -0600

    better test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using PkEisensteinHu
 using Test
+using DelimitedFiles
 
 @testset "PkEisensteinHu.jl" begin
     # compile and run test, no check for correctness, yet.
-    k = 10.0 .^ range(-5, 1, length=1000)
+    kh = 10.0 .^ range(-5, 3, length=1000)
     z=0.0
     om0=0.306819896457
     ode0=0.693155375969
@@ -16,5 +17,14 @@ using Test
     mnu=93.1e-3
     Nnu=3.046
     @show om0 + ode0
-    pkeh0 = compute_pk(k; z, om0, ode0, ob0, h0, w, ns, run, deltaR2, mnu, Nnu)
+    pk, pknw = compute_pk(kh; z, om0, ode0, ob0, h0, w, ns, run, deltaR2, mnu, Nnu)
+
+    data, header = readdlm("pk.tsv"; header=true)
+    kh0 = data[:,1]
+    pk0 = data[:,2]
+    pknw0 = data[:,3]
+
+    @test kh0 ≈ kh  rtol=1e-15
+    @test pk0 ≈ pk  rtol=1e-15
+    @test pknw0 ≈ pknw  rtol=1e-15
 end
